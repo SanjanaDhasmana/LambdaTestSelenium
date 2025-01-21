97e236b1b695d80f3fda768005b7887170d85e7c
sanjana dhasmana <sanjanadhasmana0@gmail.com>
2025-01-21 13:19:46 +0530

    added a testcase for scenario1 of Lambda test coding challenge

--- SeleniumTestLambda/UnitTest1.cs
+++ SeleniumTestLambda/UnitTest1.cs
@@ -1,16 +1,47 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
 namespace SeleniumTestLambda
 {
     public class Tests
     {
+        IWebDriver driver;
+
         [SetUp]
         public void Setup()
         {
+             driver = new ChromeDriver();
         }
 
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            driver.Navigate().GoToUrl("https://www.lambdatest.com/selenium-playground/");
+
+            IWebElement simpleFormDemoLink = driver.FindElement(By.XPath("//*[@id=\"__next\"]/div/section[2]/div/ul/li[34]/a"));
+            simpleFormDemoLink.Click();
+
+            string expected = "simple-form-demo";
+            string actual = driver.Url;
+
+            Assert.IsTrue(actual.Contains(expected));
+
+            string inputText = "Welcome to LambdaTest";
+            IWebElement enterMessageTxtBox = driver.FindElement(By.XPath("//input[@id=\"user-message\"]"));
+            enterMessageTxtBox.SendKeys(inputText);
+
+            IWebElement getCheckedValueBtn = driver.FindElement(By.XPath("//button[@id=\"showInput\"]"));
+            getCheckedValueBtn.Click();
+
+            IWebElement inputtedText = driver.FindElement(By.XPath("//*[@id=\"message\"]"));
+            Assert.That(inputText,Is.EqualTo(inputtedText.Text));
+
+
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Close();
         }
     }
 }
